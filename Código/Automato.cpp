a8f7dabdc28a63962612c1b406d4b34e62b2d8d9
Muri6-x8Y <muriloesamul@gmail.com>
2025-05-04 20:14:49 -0300

    adiciona a classe Automato finito e cria o método carregarJSON e deixa margens para implementações futuras com a variável transicoes

--- Código/Automato.cpp
+++ Código/Automato.cpp
@@ -1,7 +1,60 @@
 #include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <vector>
+#include <sstream>
+#include "json.hpp"
+
+using json = nlohmann::json;
+using namespace std;
+
+
+struct Transicao {
+    int from;
+    char read;
+    int to;
+};
+
+class AutomatoFinito {
+    int estadoInicial;
+    unordered_set<int> estadosFinais;
+    unordered_map<pair<int, char>, int, hash<string>> transicoes;
+
+    
+    struct PairHash {
+        size_t operator()(const pair<int, char>& p) const {
+            return hash<int>()(p.first) ^ hash<char>()(p.second);
+        }
+    };
+
+    unordered_map<pair<int, char>, int, PairHash> delta;
+
+public:
+    void carregarJSON(const string& arquivo) {
+        ifstream file(arquivo);
+        json j;
+        file >> j;
+
+        estadoInicial = j["inicial"];
+        for (int f : j["final"]) {
+            estadosFinais.insert(f);
+        }
+
+        for (const auto& t : j["estados"]) {
+            int from = t["from"];
+            char read = t["read"].get<string>()[0];
+            int to = t["to"];
+            delta[{from, read}] = to;
+        }
+    }
+
+};
 
 int main(){
-
+    AutomatoFinito af;
+    af.carregarJSON("automato.json");
     
     return 0;
 }
